75345be92c57eb68f49e0cbc2d329dc8a5faf40e
Aziz Jouini <aziz.jouini1998@gmail.com>
2023-08-19 12:21:56 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'John Doe',
+      bio: 'A passionate individual.',
+      imgSrc: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      profession: 'Software Developer'
+    },
+    show: false,
+    intervalId: null,
+    mountedTime: null
+  };
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  componentDidMount() {
+    this.setState({ mountedTime: new Date() });
+
+    const intervalId = setInterval(() => {
+      this.forceUpdate(); 
+    }, 1000);
+
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, mountedTime } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {show && (
+          <div className="Profile">
+            <img src={imgSrc} alt={fullName} />
+            <h1>{fullName}</h1>
+            <p>{bio}</p>
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Mounted Time: {mountedTime && mountedTime.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
